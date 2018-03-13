59ad3999a313807c4fe45371bdd341e3bc0e0705
MuhammedKhamis <muhammedessam10@gmail.com>
2018-03-13 18:10:51 +0200

    adding hello world project

---
+++ src/Lexical-Analyzer-Generator.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Lexical-Analyzer-Generator.cpp
+// Author      : Muhammed Essam Khamis
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
