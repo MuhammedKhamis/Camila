acd744616f62ec185b54d9ed74021c504b682e12
Mishors <mishors@yahoo.co.uk>
2018-03-23 13:51:29 +0200

    Bonus Files

---
+++ src/Bonus/scanner.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+int main(void)
+{
+	int ntoken;
+	ntoken = yylex();
+	while(ntoken)
+	{
+		ntoken = yylex();	
+	}
+	return 0;
+}
